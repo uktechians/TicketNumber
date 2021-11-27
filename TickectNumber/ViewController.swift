5762d3541d107bda02e675f68369ee89bbb5ec43
Urooj <uktechians@gmail.com>
2021-11-26 23:58:06 -0800

    Ticketing System First Commit

---
+++ TickectNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TickectNumber
+//
+//  Created by Uktechians on 26/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
