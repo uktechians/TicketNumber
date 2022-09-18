f91e6c67d3e7d8fac0b060a8735acdea0b10f0a2
uroojkhalidbari <Deve@1234>
2022-09-18 13:23:30 +0500

    after attaching view with class

--- TickectNumber/ViewController.swift
+++ TickectNumber/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ticketLabel: UILabel!
+    @IBOutlet weak var ticketImageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ticketLabel.text = "Ticket Number is 1"
     }
 
-
+    @IBAction func generateTicketNumber(_ sender: Any) {
+        //Print something on console
+        print("Something")
+    }
+    
 }
 
