b8fea53fd2624ffd38095e6f47c330f4b4736dea
uroojkhalidbari <Deve@1234>
2022-09-20 15:25:16 +0500

    initialization and action perform on button

--- TickectNumber/ViewController.swift
+++ TickectNumber/ViewController.swift
@@ -8,18 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let array = [#imageLiteral(resourceName: "one"), #imageLiteral(resourceName: "two"), #imageLiteral(resourceName: "three"), #imageLiteral(resourceName: "four"), #imageLiteral(resourceName: "five"), #imageLiteral(resourceName: "six")]
     @IBOutlet weak var ticketLabel: UILabel!
     @IBOutlet weak var ticketImageview: UIImageView!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         ticketLabel.text = "Ticket Number is 1"
+        count = 1
     }
 
     @IBAction func generateTicketNumber(_ sender: Any) {
         //Print something on console
-        print("Something")
+        print(count)
+        if(count < array.count){
+            ticketImageview.image = array[count]
+            count = count+1
+            ticketLabel.text = "Ticket Number is: \(count)"
+        }
+        else{
+            //resetting counter!!
+            count = 0
+        }
     }
     
 }
