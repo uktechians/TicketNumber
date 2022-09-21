e2268c770890d8018d0cb7213258b5594fbde073
uktechians <uktechians@gmail.com>
2022-09-21 21:27:08 +0500

    FInal App

--- TickectNumber/ViewController.swift
+++ TickectNumber/ViewController.swift
@@ -11,15 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var ticketLabel: UILabel!
     @IBOutlet weak var ticketImageview: UIImageView!
+    let array = [#imageLiteral(resourceName: "one"), #imageLiteral(resourceName: "two"), #imageLiteral(resourceName: "three"), #imageLiteral(resourceName: "four"), #imageLiteral(resourceName: "five"), #imageLiteral(resourceName: "six")]
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        ticketLabel.text = "Ticket Number is 1"
     }
 
     @IBAction func generateTicketNumber(_ sender: Any) {
         //Print something on console
-        print("Something")
+        print(count)
+        if(count < array.count){
+            ticketImageview.image = array[count]
+            count = count+1
+            print(count)
+            ticketLabel.text = "Ticket Number is: \(count)"
+        }
+        else {
+            count = 0
+        }
+        
     }
     
 }
